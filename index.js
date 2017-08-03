ee4f0cabfd63fcd9087f5fb026c33a6891252f1b
loatheb <zz.innj@gmail.com>
2017-08-03 23:48:58 +0800

    daily push

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-import { getParentElement, isNativeHTMLElement, $ } from './utils'
+import { getParentElement, isNativeHTMLElement, $, $$ } from './utils'
+
+function generateResult(element) {
+  const { attributes } = element
+  return [...attributes].map(function (attribute) {
+    console.log(attribute)
+    return Object.assign(Object.create(null), {
+      [attribute]: element.getAttribute(attribute)
+    })
+  })
+}
 
 class ContributionMap {
   constructor() {
     const target = $('.js-contribution-graph')
     const parent = getParentElement(target)
-    console.log(parent)
+
+    const contributions = $$('.js-calendar-graph-svg rect')
+    const contributionTree = [...contributions].map(generateResult)
+    console.log(contributionTree)
   }
 }
 
