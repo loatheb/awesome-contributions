f7e71054f5d2448aaa534df767592179def082c7
loatheb <zz.innj@gmail.com>
2017-08-04 23:45:12 +0800

    daily push

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ import { getParentElement, isNativeHTMLElement, $, $$ } from './utils'
 
 function generateResult(element) {
   const { attributes } = element
-  return [...attributes].map(function (attribute) {
-    console.log(attribute)
-    return Object.assign(Object.create(null), {
-      [attribute]: element.getAttribute(attribute)
+  return [...attributes].reduce(function(memo, current) {
+    return Object.assign({}, memo, {
+      [current.name]: current.value,
     })
-  })
+  }, Object.create(null))
 }
 
 class ContributionMap {
   constructor() {
     const target = $('.js-contribution-graph')
     const parent = getParentElement(target)
-
     const contributions = $$('.js-calendar-graph-svg rect')
     const contributionTree = [...contributions].map(generateResult)
+
     console.log(contributionTree)
+
   }
 }
 
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
+export { $, $$ } from './selector'
+
 const recursionHelper = fn => args => Array.isArray(args) ? args.map(fn) : fn(args)
 
 const isNativeHTMLElementHelper = recursionHelper(node => node.nodeType === 1)
@@ -5,5 +7,3 @@ const getParentElementHelper = recursionHelper(node => node.parentElement)
 
 export const getParentElement = nodes => getParentElementHelper(nodes)
 export const isNativeHTMLElement = nodes => isNativeHTMLElementHelper(nodes)
-
-export { $, $$ } from './selector'
